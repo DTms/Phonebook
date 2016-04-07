8a27e619b2ec87c9b69663c6b98743e0bb97b419
DTms <x00083339@gmail.com>
2016-04-07 21:13:02 +0300

    Created classes; StudentWebApiController for Web Api and StudentController

---
+++ Phonebook/Controllers/StudentController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+using Phonebook.Models;
+
+namespace Phonebook.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: All Students from DB
+        public ActionResult Index()
+        {
+            var students = DocumentDBManager<Student>.GetAllStudents();
+            return View(students);
+        }
+        // Create and save the database
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        //This cat is DocumentDB Repository and uses CreateStudentAsync method to save a new student in the database
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Surname,Name,Age,PhoneNumber,Location")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                await DocumentDBManager<Student>.CreateStudentAsync(student);
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // Editing student data and save the database
+        public ActionResult Edit(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Student student = (Student)DocumentDBManager<Student>.GetStudent(s => s.id == id);
+
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(student);
+        }
+
+
+        //This cat is DocumentDB Repository and uses UpdateStudentAsync method to update a new student in the database
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Surname,Name,Age,PhoneNumber,Location")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                await DocumentDBManager<Student>.UpdateStudentAsync(student.id, student);
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Student item = DocumentDBManager<Student>.GetStudent(s => s.id == id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(item);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed([Bind(Include = "Id")] string id)
+        {
+            await DocumentDBManager<Student>.DeleteStudentAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = DocumentDBManager<Student>.GetStudent(s => s.id == id);
+            return View(student);
+        }
+    }
+}
---
+++ Phonebook/Controllers/StudentWebApiController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Phonebook.Models;
+
+namespace Phonebook.Controllers
+{
+    public class StudentWebApiController : ApiController
+    {
+        // GET: api/StudentWebApi
+        public IEnumerable<Student> Get()
+        {
+            return DocumentDBManager<Student>.GetAllStudents();
+        }
+
+        // GET: api/StudentWebApi/5
+        public Student Get(string id)
+        {
+            return DocumentDBManager<Student>.GetStudent(s => s.id == id);
+        }
+
+        // POST: api/StudentWebApi
+        public void Post([FromBody]Student student)
+        {
+            if (!student.Equals(null))
+            {
+                DocumentDBManager<Student>.CreateStudentAsync(student);
+            }
+        }
+
+        // PUT: api/StudentWebApi/5
+        public void Put(string id, [FromBody]Student student)
+        {
+            if (!student.Equals(null) && !id.Equals(null))
+            {
+                DocumentDBManager<Student>.UpdateStudentAsync(id, student);
+            }
+        }
+
+        // DELETE: api/StudentWebApi/5
+        public void Delete(string id)
+        {
+            if (!id.Equals(null))
+            {
+                DocumentDBManager<Student>.DeleteStudentAsync(id);
+            }
+        }
+    }
+}
